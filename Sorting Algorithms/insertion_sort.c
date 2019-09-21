b42310af18ec0321710c06f4f311fcc16d8c0cc8
Tanay <tanaymehta28@gmail.com>
2019-09-21 17:36:46 +0530

    Added Boilerplate for Insertion Sort in C-lang

--- Sorting Algorithms/insertion_sort.c
+++ Sorting Algorithms/insertion_sort.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main(){
-  
+  int arr[100], n;
+
+  printf("\nPlease enter the number of elements: ");
+  scanf("%d",&n);
+
+  printf("\nEnter individual elements: ");
+  for(int i=0; i<=n; i++){
+    printf("\nEnter the (%d) element: ", i);
+    scanf("%d", &arr[i]);
+  };
+
+  // Main sorting code
+  // Coming in the next commit...
 }
